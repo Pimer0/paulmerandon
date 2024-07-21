e2537baa93b6b0af87562e420e82444fc5834e41
Pimer0 <paulmerandon@gmail.com>
2024-07-21 18:24:19 +0200

    set up du style

---
+++ app/fonts.ts
@@ -0,0 +1,6 @@
+import localFont from 'next/font/local'
+
+export const jetBrainsMono = localFont({
+  src: '../public/fonts/JetBrainsMono-2.304/fonts/webfonts/JetBrainsMono-Regular.woff2',
+  display: 'swap',
+})
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
+import { jetBrainsMono } from "../app/fonts";
 
 export default function Page() {
-  return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
-    </section>
-  )
+	return (
+		<section>
+			<h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+				Bonjour, vous êtes chez Paul Mérandon
+			</h1>
+			<p className={`mb-4 ${jetBrainsMono.className}`}>
+				{`Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam atque, veritatis culpa porro itaque eius quod quas deleniti vitae accusamus sapiente est, excepturi animi, tempore eos. Aut ad cumque sequi!`}
+			</p>
+			<div className="my-8">
+				<BlogPosts />
+			</div>
+		</section>
+	);
 }
