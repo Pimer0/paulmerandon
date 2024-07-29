36c53fd6582ada0e5f1d54ac99919d7ea02d41b6
Pimer0 <paulmerandon@gmail.com>
2024-07-29 23:59:52 +0200

    dl derniere vReact, et install de framer Motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { jetBrainsMono } from "../app/fonts";
 import { playfairDisplay } from "../app/fonts";
 import LinksProjet from "./projetCard/linksProjet";
@@ -5,27 +7,49 @@ import Projet from "./projetCard/projet";
 import TitleNivDeux from "./projetCard/TitleNivDeux";
 import next from "next";
 import Image from "next/image";
+import { animate, motion } from "framer-motion";
+import { inView } from "framer-motion";
 
 export default function Page() {
 	return (
 		<section>
-			<h1
-				className={`mb-8 text-2xl tracking-tighter ${playfairDisplay.className}`}
+			<motion.div
+				animate={{ x: [-1000, 0] }}
+				transition={{ duration: 1 }}
+				className="my-8"
 			>
-				Bonjour, vous êtes chez Paul Mérandon
-			</h1>
-			<h2
-				className={`mb-8 text-xl tracking-tighter ${playfairDisplay.className}`}
+				<h1
+					className={`mb-8 text-2xl tracking-tighter ${playfairDisplay.className}`}
+				>
+					Bonjour, vous êtes chez Paul Mérandon
+				</h1>
+			</motion.div>
+			<motion.div
+				animate={{ x: [-1000, 0] }}
+				transition={{ duration: 2 }}
+				className="my-8"
 			>
-				Un développeur à fond.
-			</h2>
-			<p className={`mb-4 ${jetBrainsMono.className}`}>
-				Bricoleur, kiffeur, aventurier, que sais-je encore… Ai-je votre
-				attention ? Si vous voulez en savoir plus sur moi, il va falloir vous
-				balader un peu sur ce site. Il y en a pour tout le monde, un peu de
-				<strong> front</strong>, un peu de <strong>back,</strong> mais aussi de{" "}
-				<strong>l'animation et de la 3D</strong>. Bienvenue chez moi !
-			</p>
+				{" "}
+				<h2
+					className={`mb-8 text-xl tracking-tighter ${playfairDisplay.className}`}
+				>
+					Un développeur à fond.
+				</h2>
+			</motion.div>
+			<motion.div
+				animate={{ x: [-1000, 0] }}
+				transition={{ duration: 3 }}
+				className="my-8"
+			>
+				{" "}
+				<p className={`mb-4 ${jetBrainsMono.className}`}>
+					Bricoleur, kiffeur, aventurier, que sais-je encore… Ai-je votre
+					attention ? Si vous voulez en savoir plus sur moi, il va falloir vous
+					balader un peu sur ce site. Il y en a pour tout le monde, un peu de
+					<strong> front</strong>, un peu de <strong>back,</strong> mais aussi
+					de <strong>l'animation et de la 3D</strong>. Bienvenue chez moi !
+				</p>
+			</motion.div>
 			<Projet
 				titre="Lexique du crédit - Cofidis"
 				srcImage="/assets/cofidis.mp4"
@@ -61,7 +85,11 @@ export default function Page() {
 				<Image width={200} height={200} src={"/assets/oeil.svg"} alt="oeil" />
 				<LinksProjet lien="/Projets/#accessibilite" />
 			</div>
-			<div className="my-8">
+			<motion.div
+				animate={{ x: [-1000, 0] }}
+				transition={{ duration: 1 }}
+				className="my-8"
+			>
 				<TitleNivDeux text="Ce portfolio !" />
 				<Image
 					width={200}
@@ -70,7 +98,7 @@ export default function Page() {
 					alt="spirale"
 				/>
 				<LinksProjet lien="/Projets/#portfolio" />
-			</div>
+			</motion.div>
 		</section>
 	);
 }
